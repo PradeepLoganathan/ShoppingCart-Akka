920bb1e9229b8446274638dca5cb00d1121b34f9
pradeepl <3273111+PradeepLoganathan@users.noreply.github.com>
2025-09-04 14:10:48 +1000

    updated endpoint

--- src/main/java/Shoppingcart/api/ShoppingCartEndpoint.java
+++ src/main/java/Shoppingcart/api/ShoppingCartEndpoint.java
@@ -7,15 +7,20 @@ import org.slf4j.LoggerFactory;
 import Shoppingcart.application.ShoppingCartEntity;
 import Shoppingcart.domain.ShoppingCart;
 import akka.javasdk.annotations.Acl;
+import akka.javasdk.annotations.http.Delete;
+import akka.javasdk.annotations.http.Get;
 import akka.javasdk.annotations.http.HttpEndpoint;
+import akka.javasdk.annotations.http.Post;
+import akka.javasdk.annotations.http.Put;
 import akka.javasdk.client.ComponentClient;
 import akka.http.javadsl.model.HttpResponse;
+import akka.javasdk.http.AbstractHttpEndpoint;
 import akka.javasdk.http.HttpResponses;
 
 
 @Acl(allow = @Acl.Matcher(principal = Acl.Principal.INTERNET))
 @HttpEndpoint("shoppingcarts")
-public class ShoppingCartEndpoint {
+public class ShoppingCartEndpoint extends AbstractHttpEndpoint {
 
     private final ComponentClient componentClient;
     private final static Logger logger = LoggerFactory.getLogger(ShoppingCartEndpoint.class.getName());
@@ -24,6 +29,7 @@ public class ShoppingCartEndpoint {
         this.componentClient = componentClient;
     }
 
+    @Put("/{cartId}/item")
     public HttpResponse addItem(String cartId, ShoppingCart.LineItem item) {
         logger.info("Received request to add item to cart: {}, item: {}", cartId, item);
         componentClient
@@ -32,6 +38,38 @@ public class ShoppingCartEndpoint {
             .invoke(item);
         return HttpResponses.ok();
     }
+
+    @Delete("/{cartId}/item/{productId}")
+    public HttpResponse removeItem(String cartId, String productId) {
+        logger.info("Received request to remove item: {} from cart: {}", productId, cartId);
+        componentClient
+            .forEventSourcedEntity(cartId)
+            .method(ShoppingCartEntity::removeItem)
+            .invoke(productId);
+        return HttpResponses.ok();
+    }
+
+    @Post("/{cartId}/checkout")
+    public HttpResponse checkout(String cartId) {
+        logger.info("Received request to checkout cart: {}", cartId);
+        componentClient
+            .forEventSourcedEntity(cartId)
+            .method(ShoppingCartEntity::checkout)
+            .invoke();
+        return HttpResponses.ok();
+    }
+
+    @Get("/{cartId}")
+    public HttpResponse getCart(String cartId) {
+        logger.info("Received request to get cart: {}", cartId);
+        ShoppingCart cart = componentClient
+            .forEventSourcedEntity(cartId)
+            .method(ShoppingCartEntity::getCart)
+            .invoke();            
+        return HttpResponses.ok(cart);
+    }
+
+    
     
 
 }
